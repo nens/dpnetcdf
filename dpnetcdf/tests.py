c0c6a98f05f979ee0770ed1a72b40c43177056f4
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-04-09 17:18:11 +0200

    Use a regular expression for parsing NetCDF filenames.

--- dpnetcdf/tests.py
+++ dpnetcdf/tests.py
@@ -10,26 +10,28 @@ from dpnetcdf.utils import parse_opendap_dataset_name
 
 TEST_DATA = {
     # key is input name, value is expected result after name is parsed
+    # DPR
     '200506010000_DPR_maas_S0v1_2015_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2015', '', 'RF1p0p3'],
+        '200506010000', 'DPR_maas', 'S0v1', '2015', None, 'RF1p0p3'],
     '200506010000_DPR_maas_S0v1_2050G_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2050G', '', 'RF1p0p3'],
+        '200506010000', 'DPR_maas', 'S0v1', '2050G', None, 'RF1p0p3'],
     '200506010000_DPR_maas_S0v1_2100G_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2100G', '', 'RF1p0p3'],
+        '200506010000', 'DPR_maas', 'S0v1', '2100G', None, 'RF1p0p3'],
     '200506010000_DPR_maas_S0v1_2100W_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2100W', '', 'RF1p0p3'],
+        '200506010000', 'DPR_maas', 'S0v1', '2100W', None, 'RF1p0p3'],
     '200506010000_DPR_rijn_S0v1_2015_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2015', '', 'RF1p0p3'],
+        '200506010000', 'DPR_rijn', 'S0v1', '2015', None, 'RF1p0p3'],
     '200506010000_DPR_rijn_S0v1_2050G_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2050G', '', 'RF1p0p3'],
+        '200506010000', 'DPR_rijn', 'S0v1', '2050G', None, 'RF1p0p3'],
     '200506010000_DPR_rijn_S0v1_2100G_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2100G', '', 'RF1p0p3'],
+        '200506010000', 'DPR_rijn', 'S0v1', '2100G', None, 'RF1p0p3'],
     '200506010000_DPR_rijn_S0v1_2100W_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2100W', '', 'RF1p0p3'],
+        '200506010000', 'DPR_rijn', 'S0v1', '2100W', None, 'RF1p0p3'],
+    # DPRD
     '199101060440_DPRD_S0v1_2015_RF1p0.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2015', '', 'RF1p0'],
+        '199101060440', 'DPRD', 'S0v1', '2015', None, 'RF1p0'],
     '199101060440_DPRD_S0v1_2015_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2015', '', 'RF1p0p3'],
+        '199101060440', 'DPRD', 'S0v1', '2015', None, 'RF1p0p3'],
     '199101060440_DPRD_S0v1_2050_RD_RF1p0p2.nc': [
         '199101060440', 'DPRD', 'S0v1', '2050', 'RD', 'RF1p0p2'],
     '199101060440_DPRD_S0v1_2050_RD_RF1p0p3.nc': [
@@ -45,7 +47,18 @@ TEST_DATA = {
     '199101060440_DPRD_S0v1_2100_SW_RF1p0p2.nc': [
         '199101060440', 'DPRD', 'S0v1', '2100', 'SW', 'RF1p0p2'],
     '199101060440_DPRD_S0v1_2100_SW_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2100', 'SW', 'RF1p0p3']
+        '199101060440', 'DPRD', 'S0v1', '2100', 'SW', 'RF1p0p3'],
+    # DPZW
+    '197612310100_DPZW_koelwater_S0v1_2015_RF1p0p4.nc': [
+        '197612310100', 'DPZW_koelwater', 'S0v1', '2015', None, 'RF1p0p4'],
+    '197612310100_DPZW_drinkwater_S0v1_2015_RF1p0p4.nc': [
+        '197612310100', 'DPZW_drinkwater', 'S0v1', '2015', None, 'RF1p0p4'],
+    '197612310100_DPZW_scheepvaart_S0v1_2015_RF1p0p4.nc': [
+        '197612310100', 'DPZW_scheepvaart', 'S0v1', '2015', None, 'RF1p0p4'],
+    '197612310100_DPZW_NHI30_AfvoerDMTakkenZomerperiode_S0v1_2050_D_RF1p0p4.nc': [
+        '197612310100', 'DPZW_NHI30_AfvoerDMTakkenZomerperiode', 'S0v1', '2050', 'D', 'RF1p0p4'],
+    '197612310100_DPZW_NHI30_WatervraagAanbodTekortKnelpuntenRegiosZomerperiode_S0v1_2050_D_RF1p0p4.nc': [
+        '197612310100', 'DPZW_NHI30_WatervraagAanbodTekortKnelpuntenRegiosZomerperiode', 'S0v1', '2050', 'D', 'RF1p0p4']
 }
 
 class OpendapTests(TestCase):
--- dpnetcdf/utils.py
+++ dpnetcdf/utils.py
@@ -2,29 +2,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import print_function
+import re
+
+
+DATASET_NAME_PATTERN = re.compile(
+    r"""^(?P<time_zero>\d{12})  # e.g. 200506010000
+         _
+         (?P<program>DP.*)  # e.g. DPR_rijn, DPRD
+         _
+         (?P<strategy>S[a-zA-Z0-9]{3})  # e.g. S0v1
+         _
+         (?P<year>\d{4}[A-Z]?)  # year can have a one character appended
+         (_(?P<scenario>[A-Z]{0,2}))?  # scenario is optional
+         _
+         (?P<calculation_facility>[A-Za-z0-9]*).nc$  # e.g. RF1p0p3
+         """, re.X)
+
 
 def parse_opendap_dataset_name(name):
-    """
-    Parse Deltaportaal Opendap file names into separate components. Very
-    custom and delicate, so test extensively.
-
-    """
-    name = name.rstrip('.nc')
-    fragments = name.split('_')
-    time_zero = fragments[0]
-    # last one is always calculation facility
-    calculation_facility = fragments[-1]
-    if fragments[-2] == 'RD' or fragments[-2] == 'SW':
-        scenario = fragments[-2]
-        year = fragments[-3]
-        strategy = fragments[-4]
-    else:
-        scenario = ''
-        year = fragments[-2]
-        strategy = fragments[-3]
-    if fragments[2].lower() == 'maas' or fragments[2].lower() == 'rijn':
-        program = "%s_%s" % (fragments[1], fragments[2])
-    else:
-        program = fragments[1]
-    return [time_zero, program, strategy, year, scenario,
-            calculation_facility]
+    """Test the regex extensively. See tests file for specific tests."""
+    res = DATASET_NAME_PATTERN.match(name).groupdict()
+    return [res.get('time_zero'), res.get('program'), res.get('strategy'),
+            res.get('year'), res.get('scenario'),
+            res.get('calculation_facility')]
