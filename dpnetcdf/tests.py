67ee03bcd1080ea2c3c633064c669fee2b7d4ac2
Roland van Laar <roland@micite.net>
2013-10-09 15:35:48 +0200

    Update name parsing NetCDF files.

--- dpnetcdf/tests.py
+++ dpnetcdf/tests.py
@@ -13,60 +13,58 @@ TEST_DATA = {
     # key is input name, value is expected result after name is parsed
     # DPR
     '200506010000_DPR_maas_S0v1_2015_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2015', None, 'RF1p0p3'],
-    '200506010000_DPR_maas_S0v1_2050G_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2050G', None, 'RF1p0p3'],
+        'S0v1', '2015'],
+    '200506010000_DPR_maas_S0v1_2050G_RF1p0p3.ns': [
+        'S0v1', '2050G'],
     '200506010000_DPR_maas_S0v1_2100G_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2100G', None, 'RF1p0p3'],
+        'S0v1', '2100G'],
     '200506010000_DPR_maas_S0v1_2100W_RF1p0p3.nc': [
-        '200506010000', 'DPR_maas', 'S0v1', '2100W', None, 'RF1p0p3'],
+        'S0v1', '2100W'],
     '200506010000_DPR_rijn_S0v1_2015_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2015', None, 'RF1p0p3'],
+        'S0v1', '2015'],
     '200506010000_DPR_rijn_S0v1_2050G_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2050G', None, 'RF1p0p3'],
+        'S0v1', '2050G'],
     '200506010000_DPR_rijn_S0v1_2100G_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2100G', None, 'RF1p0p3'],
+        'S0v1', '2100G'],
     '200506010000_DPR_rijn_S0v1_2100W_RF1p0p3.nc': [
-        '200506010000', 'DPR_rijn', 'S0v1', '2100W', None, 'RF1p0p3'],
+        'S0v1', '2100W'],
     # DPRD
     '199101060440_DPRD_S0v1_2015_RF1p0.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2015', None, 'RF1p0'],
+        'S0v1', '2015'],
     '199101060440_DPRD_S0v1_2015_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2015', None, 'RF1p0p3'],
+        'S0v1', '2015'],
     '199101060440_DPRD_S0v1_2050_RD_RF1p0p2.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2050', 'RD', 'RF1p0p2'],
+        'S0v1', '2050'],
     '199101060440_DPRD_S0v1_2050_RD_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2050', 'RD', 'RF1p0p3'],
+        'S0v1', '2050'],
     '199101060440_DPRD_S0v1_2050_SW_RF1p0p2.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2050', 'SW', 'RF1p0p2'],
+        'S0v1', '2050'],
     '199101060440_DPRD_S0v1_2050_SW_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2050', 'SW', 'RF1p0p3'],
+        'S0v1', '2050'],
     '199101060440_DPRD_S0v1_2100_RD_RF1p0p2.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2100', 'RD', 'RF1p0p2'],
+        'S0v1', '2100'],
     '199101060440_DPRD_S0v1_2100_RD_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2100', 'RD', 'RF1p0p3'],
+        'S0v1', '2100'],
     '199101060440_DPRD_S0v1_2100_SW_RF1p0p2.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2100', 'SW', 'RF1p0p2'],
+        'S0v1', '2100'],
     '199101060440_DPRD_S0v1_2100_SW_RF1p0p3.nc': [
-        '199101060440', 'DPRD', 'S0v1', '2100', 'SW', 'RF1p0p3'],
+        'S0v1', '2100'],
     # DPZW
-    '197612310100_DPZW_koelwater_S0v1_2015_RF1p0p4.nc': [
-        '197612310100', 'DPZW_koelwater', 'S0v1', '2015', None, 'RF1p0p4'],
-    '197612310100_DPZW_drinkwater_S0v1_2015_RF1p0p4.nc': [
-        '197612310100', 'DPZW_drinkwater', 'S0v1', '2015', None, 'RF1p0p4'],
-    '197612310100_DPZW_scheepvaart_S0v1_2015_RF1p0p4.nc': [
-        '197612310100', 'DPZW_scheepvaart', 'S0v1', '2015', None, 'RF1p0p4'],
-    '197612310100_DPZW_NHI30_AfvoerDMTakkenZomerperiode_S0v1_2050_D_RF1p0p4.nc': [
-        '197612310100', 'DPZW_NHI30_AfvoerDMTakkenZomerperiode', 'S0v1', '2050', 'D', 'RF1p0p4'],
-    '197612310100_DPZW_NHI30_WatervraagAanbodTekortKnelpuntenRegiosZomerperiode_S0v1_2050_D_RF1p0p4.nc': [
-        '197612310100', 'DPZW_NHI30_WatervraagAanbodTekortKnelpuntenRegiosZomerperiode', 'S0v1', '2050', 'D', 'RF1p0p4']
+    '197612310100_DPZW_koelwater_2015_RF1p0p4.nc': [
+        None, '2015'],
+    '197612310100_DPZW_drinkwater_2015_RF1p0p4.nc': [
+        None, '2015'],
+    '197612310100_DPZW_scheepvaart_2015_RF1p0p4.nc': [
+        None, '2015'],
+    '197612310100_DPZW_NHI30_AfvoerDMTakkenZomerperiode_2050_D_RF1p0p4.nc': [
+        None, '2050'],
+    '197612310100_DPZW_NHI30_WatervraagAanbodTekortKnelpuntenRegiosZomerperiode_2050_D_RF1p0p4.nc': [
+        None, '2050'],
 }
 
 
 def dataset_name_params_to_list(params):
-    return [params.get('time_zero'), params.get('program'),
-            params.get('strategy'), params.get('year'),
-            params.get('scenario'), params.get('calculation_facility')]
+    return [params.get('scenario'), params.get('year')]
 
 
 class OpendapTests(TestCase):
--- dpnetcdf/utils.py
+++ dpnetcdf/utils.py
@@ -8,17 +8,31 @@ import re
 DATASET_NAME_PATTERN = re.compile(
     r"""^(?P<time_zero>\d{12})  # e.g. 200506010000
          _
-         (?P<program>DP.*)  # e.g. DPR_rijn, DPRD
+         (?P<program>DP[a-zA-Z]*)  # e.g. DPR_rijn, DPRD
+         (_(?P<strategy>S[0-9]v[0-9]))?  # e.g. S0v1
          _
-         (?P<strategy>S[a-zA-Z0-9]{3})  # e.g. S0v1
-         _
-         (?P<year>\d{4}[A-Z]?)  # year can have a one character appended
+
          (_(?P<scenario>[A-Z]{0,2}))?  # scenario is optional
          _
          (?P<calculation_facility>[A-Za-z0-9]*).nc$  # e.g. RF1p0p3
          """, re.X)
 
 
+year = re.compile(
+    r"""
+    _(?P<year>\d{4}[A-Z]?)_  # year can have a one character appended
+    """, re.X)
+
+scenario = re.compile(
+    r"""
+    _(?P<scenario>S\dv\d)_  # scenario is optional
+    """, re.X)
+
+
 def parse_dataset_name(name):
     """Test the regex extensively. See tests file for specific tests."""
-    return DATASET_NAME_PATTERN.match(name).groupdict()
+    d = year.search(name).groupdict()
+    scenario_search = scenario.search(name)
+    if scenario_search:
+        d.update(scenario_search.groupdict())
+    return d
